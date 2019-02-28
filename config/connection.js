26737e5e2e592a88c64ca49ea5d6cbb9c23729f5
SaumilBapat <sbapat@salesforce.com>
2019-02-28 16:41:05 -0500

    db url

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "hoginogi",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.DB_URL) {
+  connection = mysql.createConnection(process.env.DB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "hoginogi",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
